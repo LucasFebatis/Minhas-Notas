f64dbe67822b9f44d91efe0e6039a4728fb98bc0
LucasFebatis <lfebatis@gmail.com>
2024-01-27 09:51:07 -0300

    add CustomAdapter

---
+++ app/src/main/java/com/febatis/minhasnotas/CustomAdapter.kt
@@ -0,0 +1,48 @@
+package com.febatis.minhasnotas
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter(private val dataSet: List<Note>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvId: TextView
+        val tvNote: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View
+            tvId = view.findViewById(R.id.tv_id)
+            tvNote = view.findViewById(R.id.tv_note)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_note, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.tvId.text = dataSet[position].uid.toString()
+        viewHolder.tvNote.text = dataSet[position].note
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
